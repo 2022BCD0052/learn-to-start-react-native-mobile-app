f22e82da154512f23778f9ef83d738ea927ff6eb
Yogesh Saini <121495438+2022BCD0052@users.noreply.github.com>
2024-12-09 15:27:20 +0530

    update

--- App.jsx
+++ App.jsx
@@ -1,6 +1,7 @@
 /* eslint-disable react-native/no-inline-styles */
 import { View, Text, Button} from 'react-native';
 import React from 'react';
+import First from './src/components/First';
 
 const App = () => {
   return (
@@ -8,6 +9,7 @@ const App = () => {
       <Text style={{ fontSize : 30}} >react native mobile app</Text>
       <Text style={{ fontSize : 30}} >react native mobile app</Text>
       <Button title='Press Here'></Button>
+      <First/>
     </View>
   );
 };
