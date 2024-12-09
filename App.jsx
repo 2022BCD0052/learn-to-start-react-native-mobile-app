01222804b8927bddea7881678657c1cde5448cc0
Yogesh Saini <121495438+2022BCD0052@users.noreply.github.com>
2024-12-09 15:01:06 +0530

    learn

---
+++ App.jsx
@@ -0,0 +1,15 @@
+/* eslint-disable react-native/no-inline-styles */
+import { View, Text, Button} from 'react-native';
+import React from 'react';
+
+const App = () => {
+  return (
+    <View>
+      <Text style={{ fontSize : 30}} >react native mobile app</Text>
+      <Text style={{ fontSize : 30}} >react native mobile app</Text>
+      <Button title='Press Here'></Button>
+    </View>
+  );
+};
+
+export default App;
