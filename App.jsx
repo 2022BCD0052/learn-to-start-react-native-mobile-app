d4aec31545e3d3985cb27da1f11a58bfcff28755
Yogesh Saini <121495438+2022BCD0052@users.noreply.github.com>
2024-12-12 01:38:34 +0530

    learn about images

--- App.jsx
+++ App.jsx
@@ -1,14 +1,20 @@
-import { StyleSheet, Text, View } from 'react-native'
+import { View, Text } from 'react-native'
 import React from 'react'
+import { createDrawerNavigator } from '@react-navigation/drawer';
+import { NavigationContainer } from '@react-navigation/native';
 
+
+const Drawer = createDrawerNavigator();
 const App = () => {
   return (
     <View>
-      <Text>App</Text>
+      <NavigationContainer>
+        <Drawer.Navigator>
+          <Drawer.Screen name = 'Home' />
+        </Drawer.Navigator>
+      </NavigationContainer>
     </View>
   )
 }
 
 export default App
-
-const styles = StyleSheet.create({})
